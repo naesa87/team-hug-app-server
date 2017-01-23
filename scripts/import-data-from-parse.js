3b95498f33c23915d2244bb673623b1040c8f9d7
Naesa Orlina <norlina@thoughtworks.com>
2017-01-23 17:03:12 +1000

    change data install to es5

--- scripts/import-data-from-parse.js
+++ scripts/import-data-from-parse.js
@@ -20,88 +20,227 @@
  * DEALINGS IN THE SOFTWARE
  */
 
-import fetch from 'isomorphic-fetch';
-import Parse from 'parse/node';
+ var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-const SERVER_PORT = process.env.PORT || 8080;
-const APP_ID = process.env.APP_ID || 'oss-f8-app-2016';
+ var importObject = function () {
+   var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(ClassType, attributes) {
+     var obj, key, value;
+     return regeneratorRuntime.wrap(function _callee$(_context) {
+       while (1) {
+         switch (_context.prev = _context.next) {
+           case 0:
+             obj = new ClassType();
+             _context.t0 = regeneratorRuntime.keys(attributes);
 
-Parse.initialize(APP_ID);
-Parse.serverURL = `http://localhost:${SERVER_PORT}/parse`;
+           case 2:
+             if ((_context.t1 = _context.t0()).done) {
+               _context.next = 11;
+               break;
+             }
 
-const BLACKLISTED_KEYS = new Set(['objectId', 'createdAt', 'updatedAt']);
-const ID_MAP = new Map();
+             key = _context.t1.value;
 
-function convertPointer(pointer) {
-  if (pointer.__type === 'Pointer') {
-    return {...pointer, objectId: ID_MAP.get(pointer.objectId) || pointer.objectId};
-  }
-  return pointer;
-}
+             if (!BLACKLISTED_KEYS.has(key)) {
+               _context.next = 6;
+               break;
+             }
 
-async function importObject(ClassType, attributes) {
-  let obj = new ClassType();
-  for (let key in attributes) {
-    if (BLACKLISTED_KEYS.has(key)) {
-      continue;
-    }
-    let value = attributes[key];
-    if (Array.isArray(value)) {
-      value = value.map(convertPointer);
-    }
-    obj.set(key, value);
-  }
-  obj = await obj.save();
-  ID_MAP.set(attributes.objectId, obj.id);
-  return obj;
-}
+             return _context.abrupt('continue', 2);
 
-async function importClass(className) {
-  console.log('Loading', className);
-  const response = await fetch(`https://api.parse.com/1/classes/${className}`, {
-    method: 'get',
-    headers: {
-      'X-Parse-Application-Id': 'R0yDMIKCUyEke2UiadcTBBGd1L5hMBTGJSdBNL3W',
-      'X-Parse-JavaScript-Key': 'BJ5V0APFMlvmCBPDXl9Mgh3q3dFrs18XkQz6A2bO',
-      'Content-Type': 'application/json'
-    },
-  });
-  const {results} = await response.json();
-  const ClassType = Parse.Object.extend(className);
-  console.log('Cleaning old', className, 'data');
-  await new Parse.Query(ClassType)
-    .each(record => record.destroy());
-  console.log('Converting', className);
-  return Promise.all(results.map(attrs => importObject(ClassType, attrs)));
-}
+           case 6:
+             value = attributes[key];
 
-var Survey = Parse.Object.extend('Survey');
+             if (Array.isArray(value)) {
+               value = value.map(convertPointer);
+             }
+             obj.set(key, value);
+             _context.next = 2;
+             break;
 
-async function createSurvey(session) {
-  return await new Survey({
-    session: session,
-    q1: 'How useful did you find the content from this session?',
-    q2: 'How likely are you to implement the products/techniques covered in this session?',
-  }).save();
-}
+           case 11:
+             _context.next = 13;
+             return obj.save();
 
-async function main() {
-  await importClass('Speakers');
-  var sessions = await importClass('Agenda');
+           case 13:
+             obj = _context.sent;
 
-  console.log('Generating sample survey questions');
-  await new Parse.Query(Survey)
-    .each(record => record.destroy());
-  await sessions.map(s => s.get('hasDetails') ? createSurvey(s) : Promise.resolve(null));
+             ID_MAP.set(attributes.objectId, obj.id);
+             return _context.abrupt('return', obj);
 
-  await Promise.all([
-    importClass('FAQ'),
-    importClass('Maps'),
-    importClass('Notification'),
-    importClass('Page'),
-  ]);
-  return 'OK';
-}
+           case 16:
+           case 'end':
+             return _context.stop();
+         }
+       }
+     }, _callee, this);
+   }));
 
-main()
-  .then(console.dir, console.error);
+   return function importObject(_x, _x2) {
+     return _ref.apply(this, arguments);
+   };
+ }();
+
+ var importClass = function () {
+   var _ref2 = _asyncToGenerator(regeneratorRuntime.mark(function _callee2(className) {
+     var response, _ref3, results, ClassType;
+
+     return regeneratorRuntime.wrap(function _callee2$(_context2) {
+       while (1) {
+         switch (_context2.prev = _context2.next) {
+           case 0:
+             console.log('Loading', className);
+             _context2.next = 3;
+             return (0, _isomorphicFetch2.default)('https://api.parse.com/1/classes/' + className, {
+               method: 'get',
+               headers: {
+                 'X-Parse-Application-Id': 'R0yDMIKCUyEke2UiadcTBBGd1L5hMBTGJSdBNL3W',
+                 'X-Parse-JavaScript-Key': 'BJ5V0APFMlvmCBPDXl9Mgh3q3dFrs18XkQz6A2bO',
+                 'Content-Type': 'application/json'
+               }
+             });
+
+           case 3:
+             response = _context2.sent;
+             _context2.next = 6;
+             return response.json();
+
+           case 6:
+             _ref3 = _context2.sent;
+             results = _ref3.results;
+             ClassType = _node2.default.Object.extend(className);
+
+             console.log('Cleaning old', className, 'data');
+             _context2.next = 12;
+             return new _node2.default.Query(ClassType).each(function (record) {
+               return record.destroy();
+             });
+
+           case 12:
+             console.log('Converting', className);
+             return _context2.abrupt('return', Promise.all(results.map(function (attrs) {
+               return importObject(ClassType, attrs);
+             })));
+
+           case 14:
+           case 'end':
+             return _context2.stop();
+         }
+       }
+     }, _callee2, this);
+   }));
+
+   return function importClass(_x3) {
+     return _ref2.apply(this, arguments);
+   };
+ }();
+
+ var createSurvey = function () {
+   var _ref4 = _asyncToGenerator(regeneratorRuntime.mark(function _callee3(session) {
+     return regeneratorRuntime.wrap(function _callee3$(_context3) {
+       while (1) {
+         switch (_context3.prev = _context3.next) {
+           case 0:
+             _context3.next = 2;
+             return new Survey({
+               session: session,
+               q1: 'How useful did you find the content from this session?',
+               q2: 'How likely are you to implement the products/techniques covered in this session?'
+             }).save();
+
+           case 2:
+             return _context3.abrupt('return', _context3.sent);
+
+           case 3:
+           case 'end':
+             return _context3.stop();
+         }
+       }
+     }, _callee3, this);
+   }));
+
+   return function createSurvey(_x4) {
+     return _ref4.apply(this, arguments);
+   };
+ }();
+
+ var main = function () {
+   var _ref5 = _asyncToGenerator(regeneratorRuntime.mark(function _callee4() {
+     var sessions;
+     return regeneratorRuntime.wrap(function _callee4$(_context4) {
+       while (1) {
+         switch (_context4.prev = _context4.next) {
+           case 0:
+             _context4.next = 2;
+             return importClass('Speakers');
+
+           case 2:
+             _context4.next = 4;
+             return importClass('Agenda');
+
+           case 4:
+             sessions = _context4.sent;
+
+
+             console.log('Generating sample survey questions');
+             _context4.next = 8;
+             return new _node2.default.Query(Survey).each(function (record) {
+               return record.destroy();
+             });
+
+           case 8:
+             _context4.next = 10;
+             return sessions.map(function (s) {
+               return s.get('hasDetails') ? createSurvey(s) : Promise.resolve(null);
+             });
+
+           case 10:
+             _context4.next = 12;
+             return Promise.all([importClass('FAQ'), importClass('Maps'), importClass('Notification'), importClass('Page')]);
+
+           case 12:
+             return _context4.abrupt('return', 'OK');
+
+           case 13:
+           case 'end':
+             return _context4.stop();
+         }
+       }
+     }, _callee4, this);
+   }));
+
+   return function main() {
+     return _ref5.apply(this, arguments);
+   };
+ }();
+
+ var _isomorphicFetch = require('isomorphic-fetch');
+
+ var _isomorphicFetch2 = _interopRequireDefault(_isomorphicFetch);
+
+ var _node = require('parse/node');
+
+ var _node2 = _interopRequireDefault(_node);
+
+ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+ function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+ var SERVER_PORT = process.env.PORT || 8080;
+ var APP_ID = process.env.APP_ID || 'oss-f8-app-2016';
+
+ _node2.default.initialize(APP_ID);
+ _node2.default.serverURL = 'http://localhost:' + SERVER_PORT + '/parse';
+
+ var BLACKLISTED_KEYS = new Set(['objectId', 'createdAt', 'updatedAt']);
+ var ID_MAP = new Map();
+
+ function convertPointer(pointer) {
+   if (pointer.__type === 'Pointer') {
+     return _extends({}, pointer, { objectId: ID_MAP.get(pointer.objectId) || pointer.objectId });
+   }
+   return pointer;
+ }
+
+ var Survey = _node2.default.Object.extend('Survey');
+
+ main().then(console.dir, console.error);
