c16da1c568c4a470bfaecf505a1f0668209f8cbd
Naesa Orlina <norlina@thoughtworks.com>
2017-01-23 13:36:08 +1000

    add dashboard

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ const SERVER_PORT = process.env.PORT || 8080;
 const SERVER_HOST = process.env.HOST || 'localhost';
 const APP_ID = process.env.APP_ID || 'oss-f8-app-2016';
 const MASTER_KEY = process.env.MASTER_KEY || '70c6093dba5a7e55968a1c7ad3dd3e5a74ef5cac';
-const DATABASE_URI = process.env.DATABASE_URI || 'mongodb://localhost:27017/dev';
+const DATABASE_URI = process.env.DATABASE_URI || 'mongodb://ds127399.mlab.com:27399/heroku_rjnj82cq';
 const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production';
 const DASHBOARD_AUTH = process.env.DASHBOARD_AUTH;
 
