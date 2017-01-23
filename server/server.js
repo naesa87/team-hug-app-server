73efdfb0ce84927346c6e2ccc5a81a7f6ebbc081
Naesa Orlina <norlina@thoughtworks.com>
2017-01-23 23:56:22 +1000

    fixed serverurl in server.js

--- server/server.js
+++ server/server.js
@@ -35,9 +35,11 @@ const MASTER_KEY = process.env.MASTER_KEY || '70c6093dba5a7e55968a1c7ad3dd3e5a74
 const DATABASE_URI = process.env.DATABASE_URI || 'localhost:27017/dev';
 const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production';
 const DASHBOARD_AUTH = process.env.DASHBOARD_AUTH;
+const SERVER_URL = process.env.SERVER_URL || 'http://localhost:' + SERVER_PORT + '/parse';
+
 
 Parse.initialize(APP_ID);
-Parse.serverURL = `http://${SERVER_HOST}:${SERVER_PORT}/parse`;
+Parse.serverURL = SERVER_URL;
 Parse.masterKey = MASTER_KEY;
 Parse.Cloud.useMasterKey();
 
@@ -60,7 +62,7 @@ server.use(
     appId: APP_ID,
     masterKey: MASTER_KEY,
     fileKey: 'f33fc1a9-9ba9-4589-95ca-9976c0d52cd5',
-    serverURL: `http://${SERVER_HOST}:${SERVER_PORT}/parse`,
+    serverURL: SERVER_URL,
   })
 );
 
@@ -69,7 +71,6 @@ if (IS_DEVELOPMENT) {
   if (DASHBOARD_AUTH) {
     var [user, pass] = DASHBOARD_AUTH.split(':');
     users = [{user, pass}];
-    console.log(users);
   }
   server.use(
     '/dashboard',
@@ -100,5 +101,5 @@ server.use(
 server.use('/', (req, res) => res.redirect('/graphql'));
 
 server.listen(SERVER_PORT, () => console.log(
-  `Server is now running in ${process.env.NODE_ENV || 'development'} mode on http://localhost:${SERVER_PORT}`
+  `Server is now running in ${process.env.NODE_ENV || 'development'} mode on ${SERVER_URL}`
 ));
